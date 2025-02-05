17353d4c987dcdaef58ee96c397b3d5304e57220
Minh Le Vu <vu25@data.cs.purdue.edu>
2025-02-05 15:48:37 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -451,6 +451,7 @@ static inline void deallocate_object(void * p) {
     //check for double free
 	if (get_state(hdr) == UNALLOCATED) {
 		printf("Double Free Detected\n");
+    #line 577
 		assert(false);
 		exit(1);
 	}
